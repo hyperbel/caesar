07f2ebc5f913f1513e59f71ea15d92b91a1bae82
hyperbel <hyperbel@users.noreply.github.com>
2022-12-19 16:48:10 +0100

    basic echo

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+  char* line;
+  size_t length = 0;
+  ssize_t lineSize = 0;
+
+  lineSize = getline(&line, &length, stdin);
+	printf("%s", line);	
+
+  free(line);
+  return 0;
+}
