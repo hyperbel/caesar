0c20a67d74c1ee3767f9d2f2497a490a9d223ff5
hyperbel <hyperbel@users.noreply.github.com>
2022-12-19 16:59:04 +0100

    continue forever

--- main.c
+++ main.c
@@ -2,20 +2,26 @@
 #include <stdlib.h>
 
 int main(void) {
-	char* line;
-	size_t length = 0;
-	ssize_t lineSize = 0;
 
-	//read stdin and get size of line
-	lineSize = getline(&line, &length, stdin);
-
-	// loop over chars in input
-	for(size_t i = 0; i < lineSize - 1; i++)
+	while (1)
 	{
-		// print char + 1 for caesar cipher
-		printf("%c", line[i] + 1);	
+		char* line;
+		size_t length = 0;
+		ssize_t lineSize = 0;
+
+		//read stdin and get size of line
+		lineSize = getline(&line, &length, stdin);
+
+		// loop over chars in input
+		for(size_t i = 0; i < lineSize - 1; i++)
+		{
+			// print char + 1 for caesar cipher
+			printf("%c", line[i] + 1);	
+		}
+
+		free(line);
+
 	}
 
-	free(line);
 	return 0;
 }
